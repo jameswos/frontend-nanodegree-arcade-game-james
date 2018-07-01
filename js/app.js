9c95d2ba8a79c91012e013d2c6bbdd9f2db84e69
James Woszczynski <jameswoszczynski@gmail.com>
2018-07-01 15:59:53 +0100

    Added Player class

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
+/*jshint esversion: 6 */
 (function() {
   "use strict";
 
-  /*jshint esversion: 6 */
   // Enemies our player must avoid
   class Enemy {
     // Variables applied to each of our instances go here,
@@ -12,7 +12,7 @@
     constructor(x, y, speed) {
       this.sprite = 'images/enemy-bug.png';
     }
-  };
+  }
 
   // Update the enemy's position, required method for game
   // Parameter: dt, a time delta between ticks
@@ -25,12 +25,16 @@
   // Draw the enemy on the screen, required method for game
   render() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-  };
+  }
 
   // Now write your own player class
   // This class requires an update(), render() and
   // a handleInput() method.
-
+  class Player {
+    constructor(x, y) {
+      this.sprite = 'images/char-boy.png';
+    }
+  }
 
   // Now instantiate your objects.
   // Place all enemy objects in an array called allEnemies
