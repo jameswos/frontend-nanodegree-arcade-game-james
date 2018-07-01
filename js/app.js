053e56e2c502550565de781f49fd1634e6afac74
James Woszczynski <jameswoszczynski@gmail.com>
2018-07-01 16:54:25 +0100

    Added Player Class and x / y positions to constructors

--- js/app.js
+++ js/app.js
@@ -1,17 +1,16 @@
 /*jshint esversion: 6 */
-(function() {
-  "use strict";
+// Enemies our player must avoid
+class Enemy {
+  // Variables applied to each of our instances go here,
+  // we've provided one for you to get started
 
-  // Enemies our player must avoid
-  class Enemy {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    constructor(x, y, speed) {
-      this.sprite = 'images/enemy-bug.png';
-    }
+  // The image/sprite for our enemies, this uses
+  // a helper we've provided to easily load images
+  constructor(x, y, speed) {
+    this.sprite = 'images/enemy-bug.png';
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
   }
 
   // Update the enemy's position, required method for game
@@ -20,40 +19,57 @@
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-  };
+  }
 
   // Draw the enemy on the screen, required method for game
   render() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
   }
+}
 
-  // Now write your own player class
-  // This class requires an update(), render() and
-  // a handleInput() method.
-  class Player {
-    constructor(x, y) {
-      this.sprite = 'images/char-boy.png';
-    }
+// Now write your own player class
+// This class requires an update(), render() and
+// a handleInput() method.
+class Player {
+  constructor(x, y) {
+    this.sprite = 'images/char-boy.png';
+    this.x = x;
+    this.y = y;
   }
 
-  // Now instantiate your objects.
-  // Place all enemy objects in an array called allEnemies
-  allEnemies = [];
-  // Place the player object in a variable called player
+  update() {
+
+  }
+
+  // Draw player on screen
+  render() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  }
+
+  handleInput() {
+
+  }
+
+}
+
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+const allEnemies = [];
+const enemy1 = new Enemy(60, 60, 100);
+allEnemies.push(enemy1);
+// Place the player object in a variable called player
+const player = new Player(10, 10);
 
 
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function(e) {
+  var allowedKeys = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down'
+  };
 
-  // This listens for key presses and sends the keys to your
-  // Player.handleInput() method. You don't need to modify this.
-  document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-      37: 'left',
-      38: 'up',
-      39: 'right',
-      40: 'down'
-    };
-
-    player.handleInput(allowedKeys[e.keyCode]);
-  });
-
-}());
+  player.handleInput(allowedKeys[e.keyCode]);
+});
